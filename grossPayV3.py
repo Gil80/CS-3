c6579f53ebec0a6ac7fa2f4702c60e09480b0ac4
gil80 <just.gil@gmail.com>
2017-10-13 01:59:00 +1100

    adding gross pay v3

---
+++ grossPayV3.py
@@ -0,0 +1,23 @@
+#  Exercise 2: Rewrite your pay program using try and except so that your
+#  program handles non-numeric input gracefully by printing a message and
+#  exiting the program.
+
+hours = input('Please enter amount of hours: ')
+try:
+    float_hours = float(hours)
+except:
+    print('Error, please enter numeric input')
+    exit()
+
+rate = input('Please enter rate per hour: ')
+try:
+    float_rate = float(rate)
+except:
+    print('Error, please enter numeric input')
+    exit()
+
+if hours > str(40):
+    grossPay = 1.5 * float_rate * float_hours
+else:
+    grossPay = float_hours * float_rate
+print('The Gross Pay Amount is:', "$"+str(round(grossPay, 2)))  # converting to string to concatenate $ sign. Adding round(answer, 2) to show 2 decimals
